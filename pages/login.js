5ffd4b1f45eb241a11cbb048b0c3d25f4f1b125f
ahnjh <algorithm123@naver.com>
2021-12-23 21:28:27 +0900

    add: login.js

---
+++ pages/login.js
@@ -0,0 +1,53 @@
+import {useState} from "react";
+import {useRouter} from "next/router";
+import axios from "axios";
+
+export default function Login() {
+
+    const router = useRouter();
+
+    const [id, setId] = useState('');
+    const [password, setPassword] = useState('');
+
+
+    return (
+        <>
+            <form onSubmit={e => {
+                (async() => {
+                    try {
+                        const response = await axios.post('/api/auth/login', {
+                            id, password
+                        });
+
+                        console.log(response);
+                        await router.push('/');
+                    } catch (e) {
+
+                    }
+                })();
+
+                e.preventDefault();
+                return false;
+            }}>
+                <h1>Login</h1>
+
+                <label>
+                    아이디<br/>
+                    <input type="text" value={id} onChange={e=> setId(e.target.value)}/>
+                </label>
+
+                <br/>
+
+                <label>
+                    비밀번호<br/>
+                    <input type="password" value={password} onChange={e => setPassword(e.target.value)}/>
+                </label>
+
+                <br/>
+
+                <button>로그인</button>
+                <button>회원가입</button>
+            </form>
+        </>
+    )
+}
