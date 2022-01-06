7f36e7598711be0b433ba1eb97f761a49ff3059d
ahnjh <algorithm123@naver.com>
2022-01-06 21:11:31 +0900

    refactor: request -> axios 로 변경

--- serverSides/logics/UpbitLogic.js
+++ serverSides/logics/UpbitLogic.js
@@ -1,46 +1,51 @@
 console.log(`${__filename}:1`);
 
-const axios = require("axios");
 
-const request = require('request');
+const axios = require("axios");
+const https = require("https");
+
 const uuidv4 = require("uuid/v4");
 const sign = require('jsonwebtoken').sign;
+
+const access_key = process.env.UPBIT_OPEN_API_ACCESS_KEY;
+const secret_key = process.env.UPBIT_OPEN_API_SECRET_KEY;
+const baseURL = process.env.UPBIT_OPEN_API_SERVER_URL;
+
+const payload = {
+    access_key: access_key,
+    nonce: uuidv4(),
+};
+
+const token = sign(payload, secret_key);
+
+
+const instance = axios.create({
+    httpsAgent: new https.Agent({   // 인증서 CA 서명 검증과정 무시
+        rejectUnauthorized: false
+    }),
+    headers: { Authorization: `Bearer ${token}` },
+    baseURL: baseURL
+});
 
 const UpbitLogic = {
 
     // 현재 갖고있는 원화 수량 반환 단위: 원
     getMyMoney: async () => {
         try {
-            const access_key = process.env.UPBIT_OPEN_API_ACCESS_KEY;
-            const secret_key = process.env.UPBIT_OPEN_API_SECRET_KEY;
-            const server_url = process.env.UPBIT_OPEN_API_SERVER_URL;
+            let myMoney = 0;
 
-            const payload = {
-                access_key: access_key,
-                nonce: uuidv4(),
-            };
+            await instance.get("/v1/accounts").then(async res => {
+                // TODO: error 처리
 
-            const token = sign(payload, secret_key);
-
-            const options = {
-                method: "GET",
-                url: server_url + "/v1/accounts",
-                headers: {Authorization: `Bearer ${token}`},
-            };
-
-            request(options, async (error, response, body) => {
-                if (error) throw new Error(error)
-
-                
-
-                await response.body.map((v, i) => {
+                await res.data.map((v, i) => {
                     if (v["currency"] === "KRW"){
-                        const myMoney = Math.floor(v["balance"]);
-                        console.log("mymone@@@@y : " + myMoney);
-                        return myMoney;
+                        myMoney = Math.floor(v["balance"]);
+                        console.log("myMoney :: " + myMoney);
                     }
                 });
-            });
+            })
+
+            return myMoney;
         } catch (e) {
             console.log("error: ");
             console.log(e);
